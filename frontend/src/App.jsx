30c9e034246adb15e713daa9c14e5832fbf13be6
pravin-rathod-git <pr819270@gmail.com>
2025-08-13 22:32:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="https://vybesocialmedia.onrender.com"
+export const serverUrl="https://newsocial-media-website.onrender.com"
 function App() {
    getCurrentUser()
    getSuggestedUsers()

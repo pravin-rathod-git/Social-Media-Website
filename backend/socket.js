ebd3f810fbee267adf4fe6c08c9211683a9a2efa
Pravin Rathod <pr819270@gmail.com>
2026-05-01 19:30:12 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"https://vybegosocial.onrender.com",
+        origin:"url",
         methods:["GET","POST"]
     }
 })
